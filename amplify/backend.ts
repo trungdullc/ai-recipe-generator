68492e1ed7770339599ff958c4b89cf000cbbc08
Trung Du <duprogramllc@gmail.com>
2025-04-10 09:51:00 -0500

    connect to bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,31 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { data } from "./data/resource";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { auth } from "./auth/resource";
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+// Updated us-east-2 since my thing authorized on us-east-2
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  "bedrockDS",
+  "https://bedrock-runtime.us-east-2.amazonaws.com",
+  {
+    authorizationConfig: {
+      signingRegion: "us-east-2",
+      signingServiceName: "bedrock",
+    },
+  }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      "arn:aws:bedrock:us-east-2::foundation-model/anthropic.claude-3-haiku-20240307-v1:0",
+    ],
+    actions: ["bedrock:InvokeModel"],
+    
+  })
+);
